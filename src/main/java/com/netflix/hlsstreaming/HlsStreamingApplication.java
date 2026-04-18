4523ca1bd9e289260025bb134afb79c787a024ed
Rukshesh Talwar <rukshesh.talwar@sleepcountry.ca>
2026-04-18 20:17:11 +0530

    Initial commit

---
+++ src/main/java/com/netflix/hlsstreaming/HlsStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.hlsstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlsStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HlsStreamingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netflix/hlsstreaming/HlsStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.hlsstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlsStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
